80b2fb6980a73d6e5477fffe082fe29f4ead0b2c
idi <idriscreate@gmail.com>
2023-05-03 17:15:02 +0100

    finished the burger nav

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunc() {
+    var x =
+document.getElementById("showHide");
+    if (x.style.display === "none") {
+        x.style.display = "block"
+    } else {
+        x.style.display = "none"
+    }
+}
+
+function boxFunc() {
+    var x =
+document.getElementById("sbox");
+    if (x.style.display === "none") {
+        x.style.display = "block"
+    } else {
+        x.style.display = "none"
+    }
+}
+
+function btnFunc() {
+    var x =
+document.getElementById("btnbox");
+    if (x.style.display === "none") {
+        x.style.display = "block"
+    } else {
+        x.style.display = "none"
+    }
+}
